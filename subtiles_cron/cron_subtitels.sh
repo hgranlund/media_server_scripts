181245e085d1f83ea78dc0c7d5aae0b6415e37b1
Simen Haugerud Granlund <simen@hgranlund.com>
2015-05-01 17:29:33 +0200

    chaged log file location on cron subtitles

--- subtiles_cron/cron_subtitels.sh
+++ subtiles_cron/cron_subtitels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat /dev/null > /home/simenhg/.log/cron_subtitels.log
+cat /dev/null > /var/log/cron_subtitels
 
 ##find /mnt/* -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)' -exec /usr/local/bin/subliminal  -l en no  --log-file /home/simenhg/.log/cron_subtitels.log -- '{}' > /home/simenhg/.log/cron_test.txt \;
 find /mnt/* -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)' -exec /usr/local/bin/subliminal  -l en no  -- '{}' > /home/simenhg/.log/cron_test.txt \;
