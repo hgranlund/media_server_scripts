fef88bcc96f9152c4aa9e7a8afbb67d402cdbb3e
Simen Haugerud Granlund <simen@hgranlund.com>
2015-05-01 17:28:20 +0200

    added cron_subtitels

---
+++ subtiles_cron/cron_subtitels.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cat /dev/null > /home/simenhg/.log/cron_subtitels.log
+
+##find /mnt/* -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)' -exec /usr/local/bin/subliminal  -l en no  --log-file /home/simenhg/.log/cron_subtitels.log -- '{}' > /home/simenhg/.log/cron_test.txt \;
+find /mnt/* -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)' -exec /usr/local/bin/subliminal  -l en no  -- '{}' > /home/simenhg/.log/cron_test.txt \;
+
